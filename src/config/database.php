027910e06ca39dc5286c3baab184fb4377aa151d
Jaider Lopez <ljaider920@gmail.com>
2025-04-25 23:01:53 -0500

    sesion 2

---
+++ src/config/database.php
@@ -0,0 +1,18 @@
+<?php 
+
+  $host       ="localhost";
+  $port       ="5432";
+  $dbname     ="energy_consumption";
+  $user       ="postgres";
+  $password   ="postgres";
+
+  $data_connection="
+     host=$host
+     port=$port
+     dbname=$dbname
+     user=$user
+     password=$password
+  ";
+
+  $conn = pg_connect($data_connection);
+?>
---
+++ src/login.php
@@ -0,0 +1,43 @@
+<?php 
+   include('config/database.php');
+
+   $nom   = $_POST['n_om'];
+   $ape   = $_POST['a_pe'];
+   $tel   = $_POST['t_el'];
+   $email = $_POST['e_mail'];
+   $pass  = $_POST['p_ass'];
+
+   // INCRIPTAR CONTRASEÑA
+   $hashed_password = password_hash($pass, PASSWORD_DEFAULT);
+   
+   $sql_validate_email ="
+       select
+         COUNT(id) as total
+       from
+         usuarios
+       where email = '$email'
+       and status = true
+   ";
+   $ans = pg_query($conn, $sql_validate_email);
+
+   if($ans){
+    $row = pg_fetch_assoc($ans);
+    if($row['total'] > 0){
+      echo "User already exists !!!";  
+    }else{
+        $sql = "INSERT INTO usuarios
+        (nombre, apellido, telefono, email, password)
+        VALUES ('$nom','$ape','$tel','$email','$hashed_password')
+        ";
+    $ans = pg_query($conn, $sql);
+    if($ans){
+     echo "<script>alert('User has created. Go to login')";
+     header('Refresh:0;URL=http://localhost/energy-consumption/src/login.html');
+    }else{
+        echo "Error";
+    }    
+    }
+   }else{
+    echo "Query error"; 
+   }
+?>
